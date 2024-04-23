2f9ccc614f8a1bc1a72070cee07d1704ff2dae7c
Pratik Jadhav <prateek7120@gmail.com>
2024-04-23 14:50:55 +0530

    DONE

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+let  js_form=document.querySelector("form");
+js_form.addEventListener("submit",formSubmitted);
+
+function formSubmitted(e){
+    e.preventDefault();
+    let titleText=document.querySelector("#title").value;
+    let authorText=document.querySelector("#author").value;
+    let isbnText=document.querySelector("#isbn").value;
+    let  table_js= document.querySelector("table");
+    let row=document.createElement('tr');
+    let cell1=document.createElement('td');
+    let cell2=document.createElement('td');
+    let cell3=document.createElement('td');
+    cell1.innerHTML=titleText;
+    cell2.innerHTML=authorText;
+    cell3.innerHTML=isbnText;
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+    table_js.appendChild(row);
+}
